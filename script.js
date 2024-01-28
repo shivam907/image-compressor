09bdc648fe384145bdc27d45ef326b09846bbe53
shivam907 <shivamkaushal907@gmail.com>
2024-01-28 22:30:01 +0530

    all done

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const MAX_WIDTH = 500;
 const MAX_HEIGHT = 500;
 const MIME_TYPE = "image/jpeg";
-const QUALITY = 0.7;
+const QUALITY = document.querySelector(".q2").textContent;
 
 const input = document.getElementById("img-input");
 input.onchange = function (ev) {
   document.querySelector(".convert").classList.remove("op");
   const file = ev.target.files[0]; // get the file
   const blobURL = URL.createObjectURL(file);
-  console.log(blobURL);
+  // console.log(blobURL);
   const img = new Image();
   img.src = blobURL;
   img.onerror = function () {
@@ -16,6 +16,7 @@ input.onchange = function (ev) {
     console.log("Cannot load image");
   };
   img.onload = function () {
+    document.querySelector(".down").classList.remove("none")
     URL.revokeObjectURL(this.src);
     const [newWidth, newHeight] = calculateSize(img, MAX_WIDTH, MAX_HEIGHT);
     const canvas = document.createElement("canvas");
@@ -28,26 +29,30 @@ input.onchange = function (ev) {
     let xhr = new XMLHttpRequest();
     xhr.responseType = "blob";
     xhr.onload = function () {
-      let a = document.createElement("a");
-      let im = document.createElement("img");
+      let a = document.querySelector(".link");
+      let im = document.querySelector(".dimg");
       im.src = window.URL.createObjectURL(xhr.response);
-      im.style.width = 500;
-      im.style.height = 500;
-      document.body.appendChild(im);
+      // im.style.width = 500;
+      // im.style.height = 500;
+      // document.body.appendChild(im);
       a.href = window.URL.createObjectURL(xhr.response);
-      a.download = file.name;
-      a.innerText = "Download";
-      console.log(a);
-      document.body.appendChild(a);
+      a.download=file.name
+      document.querySelector(".fnm").textContent = file.name;
+      // a.innerText = "Download";
+      console.log(a.download);
+      // document.body.appendChild(a);
     };
     xhr.open("GET", canvasImage);
     xhr.send();
     document.querySelector(".convert").classList.add("op");
     canvas.toBlob(
       (blob) => {
-        console.log(blob);
-        displayInfo("Original file", file);
-        displayInfo("Compressed file", blob);
+        // console.log(blob);
+        let a= displayInfo("Original file", file);
+        let b=displayInfo("Compressed file", blob, true);
+        console.log(b)
+        document.querySelector(".cp1").textContent=a;
+        document.querySelector(".cp2").textContent=b;
       },
       MIME_TYPE,
       QUALITY
@@ -72,15 +77,16 @@ function calculateSize(img, maxWidth, maxHeight) {
   return [width, height];
 }
 
-function displayInfo(label, file) {
-  const p = document.createElement("p");
-  p.innerText = `${label} - ${readableBytes(file.size)}`;
-  document.getElementById("root").append(p);
+function displayInfo(label, file, is=false) {
+  // const p = document.createElement("p");
+  return readableBytes(file.size, is);
+  // document.getElementById("root").append(p);
 }
 
-function readableBytes(bytes) {
+function readableBytes(bytes, is) {
   const i = Math.floor(Math.log(bytes) / Math.log(1024)),
     sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    if(is) return ((bytes / Math.pow(1024, i))*1.5).toFixed(2) + " " + sizes[i];
   return (bytes / Math.pow(1024, i)).toFixed(2) + " " + sizes[i];
 }
 const sliderEl = document.querySelector("#range");
@@ -88,7 +94,29 @@ const sliderValue = document.querySelector(".value");
 
 sliderEl.addEventListener("input", (event) => {
   const tempSliderValue = event.target.value;
-  sliderValue.textContent = tempSliderValue;
+  document.querySelector(".q2").textContent = tempSliderValue;
+  if (tempSliderValue >= 0.9 || tempSliderValue <= 0.1) {
+    const e = document.querySelector("#qq");
+    e.classList.remove("q4");
+    e.classList.remove("q5");
+    e.classList.add("q3");
+    e.textContent = "(Not Recommended)";
+  } else if (
+    (tempSliderValue > 0.1 && tempSliderValue < 0.4) ||
+    (tempSliderValue > 0.6 && tempSliderValue < 0.9)
+  ) {
+    const e = document.querySelector("#qq");
+    e.classList.remove("q3");
+    e.classList.remove("q4");
+    e.classList.add("q5");
+    e.textContent = "(Average)";
+  } else {
+    const e = document.querySelector("#qq");
+    e.classList.remove("q3");
+    e.classList.remove("q5");
+    e.classList.add("q4");
+    e.textContent = "(Recommended)";
+  }
   const progress = (tempSliderValue / sliderEl.max) * 100;
   sliderEl.style.background = `linear-gradient(to right, #333 ${progress}%, #ccc ${progress}%)`;
 });
